e56ad9119b8a85625dba820062fd287b6fad5aea
Amon Benson <amonkbenson@gmail.com>
2026-02-25 14:07:45 +0100

    added tauri initial project

--- sw/src/theme/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod config;
-pub mod widget;
-
-use iced::Theme;
-
-pub fn theme() -> Theme {
-    Theme::custom(String::from("Expresso"), config::PALETTE)
-}
--- sw/src/theme/widget/mod.rs
+++
@@ -1,9 +0,0 @@
-mod text;
-mod text_input;
-mod pick_list;
-mod knob;
-
-pub use text::*;
-pub use text_input::*;
-pub use pick_list::*;
-pub use knob::*;
